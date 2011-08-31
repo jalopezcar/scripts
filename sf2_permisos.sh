89b09251e0e1038b270c0d6b11f1459066c25ef2
Juan Antonio <phpjal@gmail.com>
2011-08-31 22:40:14 +0200

    permisos

--- sf2_permisos.sh
+++ sf2_permisos.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+
+echo
+
+
+APACHE_USER=$(ps axho user,comm|grep -E "httpd|apache"|uniq|grep -v "root"|awk 'END {print $1}')
+if test -z "$APACHE_USER"
+then
+    echo "Error: apache user not found "$APACHE_USER
+    exit
+else
+    echo "Apache user: "$APACHE_USER
+fi
+
+WHOAMI=$(whoami)
+if test -z "$WHOAMI"
+then
+    echo "Error: whoami fails"
+    exit
+else
+    echo "Default user "$WHOAMI
+fi
+
+set -o verbose
+
 rm -rf app/cache/*
 rm -rf app/logs/*
-sudo chmod +a "_www allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
-sudo chmod +a "jalopez allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
-
+sudo chmod +a "$APACHE_USER allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
+sudo chmod +a "$WHOAMI allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
