3bc8ae396ce17c389791527465fde4b78a56c13c
Juan Antonio <phpjal@gmail.com>
2013-03-28 16:43:23 +0100

    install

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 ## Install ##
 
-# Common
-sudo easy_install Pygments
-
 # Linux
 sudo apt-get install ccze colordiff pv
 
 # MAC
 sudo port install pv colordiff
+
+# Common
+sudo easy_install Pygments
+ln -s scripts/colorizrc .colorizrc
 
 # include in .gitconfig
 [include]
