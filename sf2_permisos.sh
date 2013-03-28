02fc1a0a5a84497a2d82ad2bec0ef1c9fb6ec712
phpjal@gmail.com <phpjal@gmail.com>
2013-03-28 15:48:12 +0100

    sf

--- sf2_permisos.sh
+++ sf2_permisos.sh
@@ -2,7 +2,6 @@
 
 echo
 
-
 APACHE_USER=$(ps axho user,comm|grep -E "httpd|apache"|uniq|grep -v "root"|awk 'END {print $1}')
 if test -z "$APACHE_USER"
 then
@@ -25,5 +24,13 @@ set -o verbose
 
 rm -rf app/cache/*
 rm -rf app/logs/*
-sudo chmod +a "$APACHE_USER allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
-sudo chmod +a "$WHOAMI allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
+
+OS=`uname -s`
+if [[ "$OS" == "Linux" ]]; then
+	sudo setfacl -R -m u:$APACHE:rwx -m u:$WHOAMI:rwx app/cache app/logs
+    sudo setfacl -dR -m u:$APACHE:rwx -m u:$WHOAMI:rwx app/cache app/logs
+else 
+	sudo chmod +a "$APACHE_USER allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
+	sudo chmod +a "$WHOAMI allow delete,write,append,file_inherit,directory_inherit" app/cache app/logs
+fi
+
